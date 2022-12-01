642826ef57c2d336f0b08ad347c6d4cf7f2e12bb
Leonardo A <l.arroyo@irya.unam.mx>
2022-12-01 11:02:51 -0600

    Personalizacion de estilos y visualización

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,7 @@
-<a href="/">
-    <i class="fa-regular fa-database fa-3x text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-sky-700"></i>
-</a>
+<div class="text-center">
+    <a href="/">
+        <i class="fa-regular fa-database fa-3x text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-sky-700"></i>
+        <br><span class="text-2xl bg-slate-800">SIIAP</span>
+    </a>
+</div>
+
