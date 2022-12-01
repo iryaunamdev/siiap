8d6c1d67b3ec4d8142bf363618fe4c8d0c6fcbc8
Leonardo A <l.arroyo@irya.unam.mx>
2022-12-01 17:34:05 -0600

    Personalizacion de estilos y visualización

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'SIIAP') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -22,18 +22,19 @@
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <aside class="w-64 block" aria-label="Sidebar">
+
+                </aside>
+
+                <section class="block">
+                    {{ $slot }}
+
+                    <footer>
+
+                    </footer>
+                </section>
             </main>
         </div>
 
--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,2 @@
-<i class="fa-regular fa-database text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-sky-700"></i>
+<i class="fa-regular fa-database fa-3x text-transparent bg-clip-text bg-gradient-to-r from-cyan-400 to-sky-700 mr-3"></i>
+<span class="inline-block align-middle text-3xl">SIIAP</span>
